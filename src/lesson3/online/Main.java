c791620bacf7d798475e46470582283e043788d8
Vladimir Stepanov <vladimir-stepanov-home@yandex.ru>
2021-03-25 10:34:10 +0300

    Реализованы примеры с классной работы.

--- src/lesson3/online/Main.java
+++ src/lesson3/online/Main.java
@@ -1,2 +1,32 @@
-package lesson3.online;public class Main {
+package lesson3.online;
+
+import java.io.File;
+import java.io.FilenameFilter;
+
+public class Main {
+    public static void main(String[] args) {
+
+        // Получаем нужную папку
+        File fileSrc = new File("folder/b");
+
+        // Создаём фильтр
+        FilenameFilter filter = new FilenameFilter() {
+
+            /**
+             * Применяет фильтр к текущему файлу
+             * @param dir Родительская директория, в которой лежит этот файл
+             * @param name Имя текущего файла
+             * @return Условие, которому должно удовлетоврять имя файла
+             */
+            @Override
+            public boolean accept(File dir, String name) {
+                return name.startsWith("cat");
+            }
+        };
+
+        // Получаем массив файлов из этой директории с применением фильтра
+        File[] files = fileSrc.listFiles(filter);
+
+    }
+
 }
--- src/lesson3/online/NIOMain.java
+++ src/lesson3/online/NIOMain.java
@@ -1,2 +1,19 @@
-package lesson3.online;public class NIOMain {
+package lesson3.online;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class NIOMain {
+    public static void main(String[] args) throws IOException {
+        Path path = Paths.get("folder/b/c/cat1.txt");
+
+        List<String> lines = Files.readAllLines(path);
+
+        new String(Files.readAllBytes(path));
+
+        Files.write(path, "Hello!".getBytes());
+    }
 }
