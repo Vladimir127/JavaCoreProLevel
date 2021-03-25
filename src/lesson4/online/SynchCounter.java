9ff843f0b55b533d71d266e305610fcd6d9ce5b9
Vladimir Stepanov <vladimir-stepanov-home@yandex.ru>
2021-03-25 18:13:07 +0300

    Реализованы примеры с классной работы.

--- src/lesson4/online/SynchCounter.java
+++ src/lesson4/online/SynchCounter.java
@@ -1,2 +1,21 @@
-package lesson4.online;public class SynchCounter {
+package lesson4.online;
+
+public class SynchCounter {
+    int c;
+
+    public int value() {return c;}
+
+    public SynchCounter() {
+        c = 0;
+    }
+
+    public void inc(){
+        synchronized (this){
+            c++;
+        }
+    }
+
+    public synchronized void dec(){
+        c--;
+    }
 }
